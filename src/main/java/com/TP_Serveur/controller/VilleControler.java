321b96601f1dd887635a4a9b24676de7ae41fc2b
sebhbt <hbt.sebastien@gmail.com>
2020-11-10 17:34:03 +0100

    Création de la fonction qui récupère les villes depuis la bdd

--- src/main/java/com/TP_Serveur/controller/VilleControler.java
+++ src/main/java/com/TP_Serveur/controller/VilleControler.java
@@ -1,21 +1,29 @@
 package com.TP_Serveur.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.TP_Serveur.dao.VilleDAO;
+import com.TP_Serveur.dao.VilleDAOImpl;
+import com.TP_Serveur.dto.Ville;
+
 @RestController
 public class VilleControler {
 	
+	@Autowired
+	VilleDAO villeDao;
+	
 	@RequestMapping(value = "/ville", method = RequestMethod.GET)
 	@ResponseBody
-	public String appelGet() {
+	public ArrayList<Ville> appelGet() {
 		System.out.println("Appel Get");
 		
-		//TODO
-		
-		return "JSON";
+		return villeDao.findAllVilles();
 	}
 	
 
--- src/main/java/com/TP_Serveur/dao/VilleDAOImpl.java
+++ src/main/java/com/TP_Serveur/dao/VilleDAOImpl.java
@@ -1,5 +1,11 @@
 package com.TP_Serveur.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Repository;
@@ -11,9 +17,74 @@ public class VilleDAOImpl implements VilleDAO {
 
 	public ArrayList<Ville> findAllVilles() {
 		ArrayList<Ville> listVille = new ArrayList<Ville>();
-		
-		//TODO récupération et req sql
-		
+
+		Connection con = null;
+		ResultSet résultats = null;
+		String requete = "";
+
+		// chargement du pilote
+		try {
+			Class.forName("org.h2.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+			System.out.println("Impossible de charger le pilote org.h2");
+		}
+
+		// connection a la base de données
+
+		System.out.println("Connexion a la base de données");
+		try {
+
+			String DBurl = "jdbc:h2:tcp://localhost/~/test";
+			con = DriverManager.getConnection(DBurl, "sa", "");
+		} catch (SQLException e) {
+			System.out.println(e);
+			System.out.println("Connection à la base de données impossible");
+		}
+
+		try {
+			Statement stmt = con.createStatement();
+			boolean nbMaj = stmt.execute(requete);
+			System.out.println("Resultat : " + nbMaj);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// creation et execution de la requete
+		System.out.println("Creation et execution de la requête");
+		requete = "SELECT * FROM VILLE_FRANCE";
+
+		try {
+			Statement stmt = con.createStatement();
+			résultats = stmt.executeQuery(requete);
+		} catch (SQLException e) {
+			System.out.println("Anomalie lors de l'execution de la requête");
+		}
+
+		// parcours des données retournées
+		System.out.println("Parcours des données retournées");
+		try {
+			ResultSetMetaData rsmd = résultats.getMetaData();
+			int nbCols = rsmd.getColumnCount();
+			boolean encore = résultats.next();
+
+			while (encore) {
+				Ville ville = new Ville();
+				ville.setCodeCommune(résultats.getString(1));
+				ville.setNomCommune(résultats.getString(2));
+				ville.setCodePostal(résultats.getString(3));
+				ville.setLibelleAcheminement(résultats.getString(4));
+				ville.setLigne(résultats.getString(5));
+				
+				listVille.add(ville);
+				
+				encore = résultats.next();
+			}
+
+			résultats.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return listVille;
 	}
 }
